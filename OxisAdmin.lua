677eb2b4a9bf318917b48eb5b4dcc27f09b26c7f
Oxisist <93292992+Oxisist@users.noreply.github.com>
2022-02-20 19:52:20 -0600

    Update OxisAdmin.lua

--- OxisAdmin.lua
+++ OxisAdmin.lua
@@ -147,8 +147,18 @@ plr.Chatted:connect(function(message)
     end
     
     if message:sub(1,3) == ".hop" then
-        game:GetService("TeleportService"):Teleport(gameid, game:GetService("Players").LocalPlayer);
-    end
+   local x = {}
+	for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+		if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+			x[#x + 1] = v.id
+		end
+	end
+	if #x > 0 then
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+	else
+		return notify("Serverhop","Couldn't find a server.")
+	end
+end)
     
     if message:sub(1,8) == ".lowhold" then
     	if plr.Character:FindFirstChild("Animate").Disabled == true then return end
